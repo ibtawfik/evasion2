86ebb558873d7a891f8747c0990c3fe7fda9ca6c
Islam Tawfik <ibtawfik@gmail.com>
2015-11-06 07:47:47 -0500

    initial, ariels work

---
+++ src/main/java/Client.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by islam on 11/2/15.
+ */
+public class Client {
+//    public static void main(String[] args) throws IOException {
+//        Socket socket = new Socket("localhost", 1990);
+//        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//        String command;
+//
+//
+//
+//        out.println(" {\n" +
+//                "   \"command\":\"B\",\n" +
+//                "   \"wall\": {\n" +
+//                "     \"length\": 10,\n" +
+//                "     \"direction\": \"N\"\n" +
+//                "   }\n" +
+//                "  }");
+//        while ((command = in.readLine()) != null) {
+//            System.out.println("Received:" + command);
+//            //String reply = "hello from client 3";
+////            if (!reply.equals("")) {
+////                out.println(reply);
+////            }
+//
+//        }
+//    }
+//}
+
+
+}
